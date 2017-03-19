c97d9f84a112613db81aeb6c4b06b058af517b80
inabajunmr <inabajunmr@192.168.179.4>
2017-03-19 12:14:56 +0900

    プロジェクト新規作成

---
+++ src/main/java/homepagewrecker/HomepageWreckerApplication.java
@@ -0,0 +1,12 @@
+package homepagewrecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomepageWreckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomepageWreckerApplication.class, args);
+	}
+}

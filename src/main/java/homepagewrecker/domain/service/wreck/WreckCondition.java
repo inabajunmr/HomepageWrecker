c7701389df1a58e371dfd58208372de15ab91e69
inabajunmr <inabajunmr@192.168.179.4>
2017-03-19 14:33:37 +0900

    html破壊部分実装中 jsoupを依存関係に追加

--- src/main/java/homepagewrecker/domain/service/wreck/WreckCondition.java
+++ src/main/java/homepagewrecker/domain/service/wreck/WreckCondition.java
@@ -1,5 +1,17 @@
 package homepagewrecker.domain.service.wreck;
 
+import java.net.URL;
+
 public class WreckCondition {
+	public WreckCondition(URL sourceUrl) {
+		super();
+		this.sourceUrl = sourceUrl;
+	}
+
+	public URL getSourceUrl() {
+		return sourceUrl;
+	}
+
+	private URL sourceUrl;
 
 }
--- src/main/java/homepagewrecker/domain/service/wreck/impl/HtmlWreckerImpl.java
+++ src/main/java/homepagewrecker/domain/service/wreck/impl/HtmlWreckerImpl.java
@@ -1,14 +1,35 @@
 package homepagewrecker.domain.service.wreck.impl;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
+
+import org.springframework.stereotype.Service;
 
 import homepagewrecker.domain.service.wreck.HtmlWrecker;
 import homepagewrecker.domain.service.wreck.WreckCondition;
+import relative_to_absolute_path_html.converter.ConvertCondition;
+import relative_to_absolute_path_html.converter.Converter;
+import relative_to_absolute_path_html.converter.impl.ConverterImpl;
+import relative_to_absolute_path_html.reader.ReadTargetCondition;
+import relative_to_absolute_path_html.reader.Reader;
+import relative_to_absolute_path_html.reader.impl.ReaderImpl;
 
+@Service
 public class HtmlWreckerImpl implements HtmlWrecker{
+
+	Reader reader = new ReaderImpl();
+	Converter converter = new ConverterImpl();
 
 	@Override
 	public String wreck(Path path, WreckCondition cond) {
+		try {
+			String html = reader.read(new ReadTargetCondition(path, StandardCharsets.UTF_8));
+			String convertHtml = converter.convert(html, new ConvertCondition(cond.getSourceUrl()));
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
 		// TODO 自動生成されたメソッド・スタブ
 		return null;
 	}
